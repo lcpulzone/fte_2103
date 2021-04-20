610fb401bebd144339afd27d883035c6db3ba862
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-20 12:17:55 -0700

    Create spec for food truck, begin initial class build

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,38 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  context 'initialization' do
+    it 'exists' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+    end
+  end
+end
+# item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+# item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+# food_truck.name
+# #=> "Rocky Mountain Pies"
+#
+# food_truck.inventory
+# #=> {}
+#
+# food_truck.check_stock(item1)
+# #=> 0
+#
+# food_truck.stock(item1, 30)
+#
+# food_truck.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 30}
+#
+# food_truck.check_stock(item1)
+# #=> 30
+#
+# food_truck.stock(item1, 25)
+#
+# food_truck.check_stock(item1)
+# #=> 55
+#
+# food_truck.stock(item2, 12)
+#
+# food_truck.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
