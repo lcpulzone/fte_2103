29cbb482e571f13793c1f447ea9326b265cddd30
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-20 12:31:15 -0700

    Finish expects for iteration 1, create stock method

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -25,30 +25,31 @@ RSpec.describe FoodTruck do
       food_truck = FoodTruck.new("Rocky Mountain Pies")
       item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
       item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-      
+
       expect(food_truck.check_stock(item1)).to eq(0)
     end
 
+    it 'can stock an item in inventory' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      food_truck.stock(item1, 30)
+
+      expect(food_truck.inventory).to eq({item1 => 30})
+      expect(food_truck.check_stock(item1)).to eq(30)
+
+      food_truck.stock(item1, 25)
+
+      expect(food_truck.check_stock(item1)).to eq(55)
+
+      food_truck.stock(item2, 12)
+
+      expected = {
+        item1 => 55,
+        item2 => 12
+      }
+      expect(food_truck.inventory).to eq(expected)
+    end
   end
 end
-
-
-
-#
-# food_truck.stock(item1, 30)
-#
-# food_truck.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 30}
-#
-# food_truck.check_stock(item1)
-# #=> 30
-#
-# food_truck.stock(item1, 25)
-#
-# food_truck.check_stock(item1)
-# #=> 55
-#
-# food_truck.stock(item2, 12)
-#
-# food_truck.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
