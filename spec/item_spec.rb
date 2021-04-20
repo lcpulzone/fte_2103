5a104ece085515d1d0562c4722735b1b897e4b82
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-20 12:14:09 -0700

    Add price spec, create price attr_reader

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -11,15 +11,15 @@ RSpec.describe Item do
     end
 
     it 'has a name' do
-      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
       item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
       expect(item2.name).to eq('Apple Pie (Slice)')
     end
+
+    it 'has a price' do
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      expect(item2.price).to eq(2.50)
+    end
   end
 end
-#
-# item2.name
-# #=> "Apple Pie (Slice)"
-#
-# item2.price
